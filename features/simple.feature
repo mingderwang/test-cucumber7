6c5ce09b7d419ec98d9d40a750f65cffa8e7d90e
Ming-der Wang <mingderwang@gmail.com>
2022-05-16 21:07:46 +0800

    Scenario in Chinese

--- features/simple.feature
+++ features/simple.feature
@@ -2,7 +2,7 @@
 @tag
 Feature: Eating too many cucumbers may not be good for you
   Eating too much of anything may not be good for you
-  Scenario: Eating a few is no problem
+  Scenario: 吃一點沒關係
     Given Alice is hungry
     When she eats 3 cucumbers
     Then she is not full
--- step-definitions/eating/test-steps.ts
+++ step-definitions/eating/test-steps.ts
@@ -1,20 +1,21 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { CustomWorld } from "../../world";
-import { SimpleOne } from "../../domains/eating/simple-one";
-import expect from "expect";
+import { Given, Then, When } from '@cucumber/cucumber'
+import { CustomWorld } from '../../world'
+import { SimpleOne } from '../../domains/eating/simple-one'
+import expect from 'expect'
 
-Given("Alice is hungry", async function (this: CustomWorld) {
-  this.instance = new SimpleOne();
-});
+Given('Alice is hungry', async function (this: CustomWorld) {
+  this.instance = new SimpleOne()
+})
 
-When("she eats {int} cucumbers", async function (this: CustomWorld, _int) {
-  this.instance.eat(_int);
-});
+When('she eats {int} cucumbers', async function (this: CustomWorld, _int) {
+  console.log(`Eating ${_int} cucumbers.`)
+  this.instance.eat(_int)
+})
 
-Then("she is not full", async function (this: CustomWorld) {
-  expect(this.instance.isFull()).toBe(false);
-});
+Then('she is not full', async function (this: CustomWorld) {
+  expect(this.instance.isFull()).toBe(false)
+})
 
-Then("she will be full", async function (this: CustomWorld) {
-  expect(this.instance.isFull()).toBe(true);
-});
+Then('she will be full', async function (this: CustomWorld) {
+  expect(this.instance.isFull()).toBe(true)
+})
