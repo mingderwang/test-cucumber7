330c68c84a78efe8f749c01ef9e3d2c9d69285b3
Ming-der Wang <mingderwang@gmail.com>
2022-05-16 21:34:15 +0800

    ok for v1.0.0 release in Chinese features

--- features/simple.feature
+++ features/simple.feature
@@ -1,10 +1,10 @@
-# Comment
+# 用範例, 且改成中文
 @tag
-Feature: Eating too many cucumbers may not be good for you
-  Eating too much of anything may not be good for you
+Feature: 吃太多小黃瓜對你沒有什麼好處
+  吃太多任何東西都對你沒什麼好處
   Scenario: 吃一點沒關係
-    Given Alice is hungry
-    When she eats 3 cucumbers
-    Then she is not full
-    When she eats 2 cucumbers
-    Then she will be full
+    Given 愛麗絲肚子有點餓
+    When 她吃了 3 條小黃瓜s
+    Then 她沒有飽
+    When 她又吃了 1 條小黃瓜
+    Then 她飽了
--- step-definitions/eating/test-steps.ts
+++ step-definitions/eating/test-steps.ts
@@ -3,19 +3,30 @@ import { CustomWorld } from '../../world'
 import { SimpleOne } from '../../domains/eating/simple-one'
 import expect from 'expect'
 
-Given('Alice is hungry', async function (this: CustomWorld) {
+import { defineParameterType } from '@cucumber/cucumber'
+// I have a {color} ball
+defineParameterType({
+  name: 'color',
+  regexp: /red|blue|yellow/,
+  transformer: (s: string | number | Date) => new Date(s),
+})
+
+Given('愛麗絲肚子有點餓', async function (this: CustomWorld) {
   this.instance = new SimpleOne()
 })
 
-When('she eats {int} cucumbers', async function (this: CustomWorld, _int) {
+When('她吃了/她又吃了 {int} 條小黃瓜(s)', async function (
+  this: CustomWorld,
+  _int,
+) {
   console.log(`Eating ${_int} cucumbers.`)
   this.instance.eat(_int)
 })
 
-Then('she is not full', async function (this: CustomWorld) {
+Then('她沒有飽', async function (this: CustomWorld) {
   expect(this.instance.isFull()).toBe(false)
 })
 
-Then('she will be full', async function (this: CustomWorld) {
+Then('她飽了', async function (this: CustomWorld) {
   expect(this.instance.isFull()).toBe(true)
 })
